0c1b2b20374adbe441feefcaf51f088333d06c4e
NeverFlyDog <217641660+NeverFlyDog@users.noreply.github.com>
2025-08-26 20:47:40 +0300

    Replace hardcoded test user credentials with system properties

--- src/test/java/com/demoqa/data/AuthData.java
+++ src/test/java/com/demoqa/data/AuthData.java
@@ -1,6 +1,6 @@
 package com.demoqa.data;
 
 public class AuthData {
-    public static final String LOGIN = "amber.roomba@example.com";
-    public static final String PASSWORD = "glKier*25oi";
+    public static final String LOGIN = System.getProperty("userLogin", null);
+    public static final String PASSWORD = System.getProperty("userPassword", null);
 }
